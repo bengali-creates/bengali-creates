5907eabb5b1d2b31d110938cc4f3c4a97e067743
bengali-creates <debasish2005pal@gmail.com>
2024-11-13 12:54:47 +0530

    fixed some issue

--- pass_genarator.py
+++ pass_genarator.py
@@ -13,7 +13,7 @@ def pass_gen(min_length,numbers=True,special_charecter=True):
     has_number=False
     has_sc=False
 
-    while not meets_criteria or len(pwd)>min_length:
+    while not meets_criteria or len(pwd)<min_length:
         new_char=random.choice(charecters)
         pwd += new_char
 
@@ -26,14 +26,16 @@ def pass_gen(min_length,numbers=True,special_charecter=True):
         
         meets_criteria= True
         
-        if number:
+        if numbers:
             meets_criteria = has_number
-        if special_char:
+        if special_charecter:
             meets_criteria = meets_criteria and has_sc
 
     return pwd
 
 
-
-y=pass_gen(20)
-print(y)
+min_length= int(input("Enter the min length of the pass: "))
+has_number=input("Enter if you want to keep numbers(y/n): ").lower()=="y"
+has_sc=input("Enter if you want to keep special char(y/n): ").lower()=="y"
+y=pass_gen(min_length,has_number,has_sc)
+print("Your genrated password is: ",y)
